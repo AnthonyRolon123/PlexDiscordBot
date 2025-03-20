6c34d3515dd3c0e59be0845728dc2660375ce431
Anthony <anthonyrolon1234@gmail.com>
2025-03-20 18:50:47 -0400

    paused play command

--- src/index.js
+++ src/index.js
@@ -67,6 +67,8 @@ configs.client.on('interactionCreate', async (interaction) => {
             interaction.followUp({content: `Pausing Movie`, ephemeral: isEphemeral  });
             break;
         case 'play':
+            await interaction.followUp({content: `not working atm`, ephemeral: isEphemeral  })
+            break;
             interaction.reply('what movie do you want to watch', {ephermal: isEphemeral});
 
             const filter = (response) => {
