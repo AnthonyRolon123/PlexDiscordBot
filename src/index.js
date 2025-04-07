5992bbd7a19909502cfe3d39c79a1bb74722f4ee
Anthony <anthonyrolon1234@gmail.com>
2025-04-07 09:22:07 -0400

    catch skip error

--- src/index.js
+++ src/index.js
@@ -135,20 +135,22 @@ configs.client.on('interactionCreate', async (interaction) => {
 });
 
 const skipMovie = async () => {
-    await axios({
-        method: 'get',
-        timeout: 2000,
-        url: `http://${process.env.IP}:${process.env.PORT}/player/playback/skipNext`,
-        headers: {
-         'X-Plex-Token': process.env.PLEX_TOKEN,
-         'Accept': 'application/json',
-        },
-        params: {
-            type: 'video',
-            commandID: 0,
-            'X-Plex-Target-Client-Identifier': process.env.PLEX_CLIENT_ID,
-        }
-    })
+    try {
+        await axios({
+            method: 'get',
+            timeout: 2000,
+            url: `http://${process.env.IP}:${process.env.PORT}/player/playback/skipNext`,
+            headers: {
+             'X-Plex-Token': process.env.PLEX_TOKEN,
+             'Accept': 'application/json',
+            },
+            params: {
+                type: 'video',
+                commandID: 0,
+                'X-Plex-Target-Client-Identifier': process.env.PLEX_CLIENT_ID,
+            }
+        })
+    } catch (error) {}
 }
 
 const previousMovie = async () => {
