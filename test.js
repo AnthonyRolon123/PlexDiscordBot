4703adddde85a7837439f4b9bbd90de319ee6881
Anthony <anthonyrolon1234@gmail.com>
2025-05-12 20:02:17 -0400

    issue with timeout

--- test.js
+++ test.js
@@ -1,75 +1,41 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import axios from 'axios';
+// import dotenv from 'dotenv';
+// dotenv.config();
+// import axios from 'axios';
 
-const main = async () => {
+// const nowPlaying = async () => {
+//     let res = await axios({
+//         timeout: 20000,
+//         method: 'get',
+//         url: `https://community.plex.tv/api/`,
+//         headers: {
+//             'X-Plex-Token': process.env.PLEX_TOKEN,
+//             'Accept': 'application/json',
+//             // 'X-Plex-Client-Identifier': process.env.PLEX_CLIENT_ID,
+//         },
+//     })
 
-    // await axios({
-    //     // timeout: 2000,
-    //     method: 'put',
-    //     url: `http://${process.env.IP}:${process.env.PORT}/sync`,
-    //     headers: {
-    //         'X-Plex-Token': process.env.PLEX_TOKEN,
-    //         'Accept': 'application/json',
-    //     },
-    //     params: {
-    //         type: 'video',
-    //         commandID: 0,
-    //         'X-Plex-Target-Client-Identifier': process.env.PLEX_CLIENT_ID,
-    //     }
-    // })
+//     return res.data.MediaContainer.Metadata[0].title;
+// }
 
-    let res = await axios({
-        method: 'get',
-        url: `http://${process.env.IP}:${process.env.PORT}/search`,
-        headers: {
-            'X-Plex-Token': process.env.PLEX_TOKEN,
-            'Accept': 'application/json',
-        },
-        params: {
-            'X-Plex-Target-Client-Identifier': process.env.PLEX_CLIENT_ID,
-            query: `the matrix`
-        }
-    })
+// // await main();
+// console.log(await nowPlaying());
 
-    //'/library/metadata/6414'
-    //ratingKey: '6414'
+let n = 5000
 
-    // let res = await axios({
-    //     // timeout: 2000,
-    //     method: 'get',
-    //     url: `http://${process.env.IP}:${process.env.PORT}/status/sessions`,
-    //     headers: {
-    //         'Accept': 'application/json',
-    //         'X-Plex-Token': process.env.PLEX_TOKEN,
-    //     },
-    //     params: {
-    //         // type: 'video',
-    //         // commandID: 0,
-    //         // offset: 0,
-    //         'X-Plex-Target-Client-Identifier': process.env.PLEX_CLIENT_ID,
-    //     }
-    // })
+let answer = 12;
+let result = n;
 
-    console.dir(res.data, {depth: 99});
-    // console.log(res.data.MediaContainer.Metadata[0]);
-    console.log('done');
-}
+// if(n === 1){
+//     return answer;
+// }
 
-const nowPlaying = async () => {
-    let res = await axios({
-        timeout: 20000,
-        method: 'get',
-        url: `http://${process.env.IP}:${process.env.PORT}/status/sessions`,
-        headers: {
-            'X-Plex-Token': process.env.PLEX_TOKEN,
-            'Accept': 'application/json',
-            'X-Plex-Client-Identifier': process.env.PLEX_CLIENT_ID,
-        },
-    })
+while(n > 0){
+    result *= n-1;
+    n = n-1;
+  }
 
-    return res.data.MediaContainer.Metadata[0].title;
-}
+// answer = 72 * result;
 
-// await main();
-console.log(await nowPlaying());
+
+
+console.log(result%Math.pow(10,9)) + 7
